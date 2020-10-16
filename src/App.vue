b740f324326ebc923ceffb00fa1838a3d9b47a9b
ArtcalO97 <niyca14@gmail.com>
2020-10-16 11:50:22 +0200

    Adding Home component , and making some nav bars

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,23 @@
 <template>
   <div id="app">
-    <app-header></app-header>
     <hr>
-    <app-course></app-course>
+    <app-home></app-home>
     <hr>
-    <app-mark></app-mark>
-    <hr>
-    <app-student></app-student>
-    <hr>
-    <app-footer></app-footer>
+
   </div>
 </template>
 
 <script>
-
-import Courses from './components/others/Courses.vue';
-import Marks  from './components/others/Marks.vue';
-import Students  from './components/others/Students.vue';
-
-import Header  from './components/shared/Header.vue';
-import Footer  from './components/shared/Footer.vue';
-
+import Home from './components/Home.vue'
 export default {
-  components : {
-    'app-course' : Courses,
-    'app-mark' : Marks,
-    'app-student' : Students,
 
-    'app-header' : Header,
-    'app-footer' : Footer
+  components : {
+    'app-home' : Home
+  },
+  data (){
+    return{
+
+    }
   }
   
 };
--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,7 +1,18 @@
 <template>
-  <div >
-    <h3>Header</h3>
-  </div>
+ <nav class="navbar navbar-expand-sm bg-light ">
+	 <ul class="nav nav-pills">
+	  <li class="nav-item">
+	    <a class="nav-link active" data-toggle="pill" href="#home">Home</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link" data-toggle="pill" href="#menu1">Menu 1</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link" data-toggle="pill" href="#menu2">Menu 2</a>
+	  </li>
+	</ul>
+</nav> 
+  
 </template>
 
 <script>
