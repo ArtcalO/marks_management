1f15f43488be5cedda272a6d832f057141782192
ArtcalO97 <niyca14@gmail.com>
2020-10-19 21:14:02 +0200

    Adding VueX in app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,7 @@
     <app-header></app-header>
     <hr>
     <div>
-      <router-view 
-        :courses="courses"
-        @courseCreated="createCourse"
-        ></router-view>
+      <router-view ></router-view>
     </div>
     <hr>
     <app-footer></app-footer>
@@ -18,20 +15,11 @@ import Header  from './components/shared/Header.vue';
 import Footer  from './components/shared/Footer.vue';
 
 export default {
-  data (){
-    return {
-      courses : ['Kirundi']
-    }
-  },
   components : {
     'app-header' : Header,
     'app-footer' : Footer
-  },
-  methods : {
-    createCourse($emit){
-      this.course.push($emit);
-    }
   }
+  
 };
 </script>
 
